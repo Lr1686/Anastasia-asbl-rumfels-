d64951091c0a9d06c8c100a4f4a8b09eacd084e6
lrcopie&collll;]1% <lionelrumfels1986@gmail.com>
2026-08-09 05:39:03 +0200

    Update gatekeeper.sh

--- gatekeeper.sh
+++ gatekeeper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 EXPECTED_HASH="7a49b6ad54d5787a1966a00aad04b542857154ca8598cf42183bb37f31ec12ba"
-ACTUAL_HASH=$(shasum -a 256 /Users/fissette/Downloads/Gemini.dmg | awk '{print $1}')
+ACTUAL_HASH=$(shasum -a 256 /Users/ANASTASIAPRIVÉ_RUMFELS_Propriétaire_de_100%_de vie_et_au-delà/Downloads/Gemini.dmg | awk '{print $1}')
 
 if [ "$EXPECTED_HASH" == "$ACTUAL_HASH" ]; then
   echo "✅ NOMAD : Intégrité certifiée. Fréquence 12_ALPHA stable."
